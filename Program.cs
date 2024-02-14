b52c647bafb7dc064cc82f5689f385e7867de501
mamedovx999 <mammadalifm@code.edu.az>
2024-02-14 19:03:56 +0400

    add

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿Console.WriteLine("Hello, World!");
+#region 
+//int n = 23;
+//int m = 30;
+//int count = 0;
+//for (int i = n; i < m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+#endregion
+
+#region task2
+//int n = 11;
+//if (n % 2 == 1 && n % n == 0)
+//{
+//    Console.WriteLine("sadedir");
+//}
+//else
+//{
+//    Console.WriteLine("Murekkebdir");
+//}
+
+#endregion
+
+#region task3
+//int a = 90;
+//int b = 10;
+//if (a % b == 0)
+//{
+//   Console.WriteLine("tam bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("bolunmur");
+//}
+
+#endregion
+
+#region task4
+//int x = 60;
+//int y = 40;
+//int z = 80;
+//int resullt = 0;
+
+//if (x > y && y > z || y > z && x > z)
+//{
+//    Console.WriteLine(resullt = x + y);
+//}
+//else if (x < y && y < z) 
+//{
+//    Console.WriteLine(resullt = z + y); 
+//}
+//else 
+//{
+//    Console.WriteLine(resullt = x + z); 
+//}
+#endregion
